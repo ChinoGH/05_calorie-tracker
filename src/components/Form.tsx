fc8310007e1a2358feaa0b1b243342e9f4c9b8f8
sfortiz <chinotr@hotmail.com>
2026-09-02 18:32:57 -0600

    Video 134. Agregando el Formulario de Entrada de datos (1 de 3)

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,19 @@
 
 export default function Form() {
   return (
-    <div>Form</div>
+    <form
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+    >
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category">Categoría:</label>
+        <select
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          id="category"
+        >
+
+        </select>
+      </div>
+
+    </form>
   )
 }
